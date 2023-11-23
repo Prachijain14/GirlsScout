e7d715d221cb83f50418d74d268aaded8df0cd9d
C_Joseph <cijo.joseph1@dellteam.com>
2023-11-23 11:57:04 +0530

    Add project files.

---
+++ GirlScoutCookieBoothManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GirlScoutCookieBoothManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GirlScoutCookieBoothManager.Web/ViewModels/ShiftDetailsVM.cs
@@ -0,0 +1,6 @@
+﻿namespace GirlScoutCookieBoothManager.Web.ViewModels
+{
+    public class ShiftDetailsVM
+    {
+    }
+}
